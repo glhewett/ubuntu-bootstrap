e2fe0d0fa1ed6a5df2fef2db95568983a5089791
Greg Hewett <ghewett@cisco.com>
2017-11-15 16:05:29 -0600

    first commit

---
+++ 0020-install_dependencies.sh
@@ -0,0 +1,8 @@
+apt-get install -y \
+  vim \
+  git \
+  tmux \
+  curl \
+  wget \
+  openssh-server \
+  build-eseential
---
+++ 0030-install_rcm.h
@@ -0,0 +1,5 @@
+wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+echo "deb http://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+sudo apt-get update
+sudo apt-get install rcm
+
